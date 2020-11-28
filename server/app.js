052095efaf5660d6d32a31bb7050d2c88eeea926
1287361614@qq.com <1287361614@qq.com>
2020-11-28 15:54:55 +0800

    three

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var indexRouter = require('./routes/index');
+var grainRouter = require('./routes/grain');
 
 var app = express();
 
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-const IndexControllers = require('../controllers/index')
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index')
 });
-
+router.get('/grain', function(req, res) {
+  res.render('grain')
+});
+router.get('/raindrop', function(req, res) {
+  res.render('grain')
+});
 
 module.exports = router;
